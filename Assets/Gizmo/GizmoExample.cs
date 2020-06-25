42f4ba74357070e93af97223ed696985d6e63dbd
og <oguogu@test.com>
2020-06-25 10:56:15 +0900

    [add] GizmoExampleを追加
    単純なキューブを表示するギズモ

--- Assets/Gizmo/GizmoExample.cs
+++ Assets/Gizmo/GizmoExample.cs
@@ -7,11 +7,20 @@ public class GizmoExample : MonoBehaviour
 {
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawCube(
+            transform.position, Vector3.one*2);
         
+        #if UNITY_EDITOR
+        UnityEditor.Handles.Label(transform.position + new Vector3(0, 3, 0), "簡単なギズモの例");
+#endif
+
     }
 
     private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(
+            transform.position, Vector3.one*2);
     }
 }
