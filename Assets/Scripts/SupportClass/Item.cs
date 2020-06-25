dd8f96643e14a1a2a0cb572d78088987dbb7fb56
og <oguogu@test.com>
2020-06-25 17:28:51 +0900

    [add] Support用クラスを追加
    実装のサンプル用に使用するクラスです。

---
+++ Assets/Scripts/SupportClass/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public enum Category
+    {
+        Collection,
+        Enemies,
+        Misc
+    }
+
+    public Category category = Category.Collection;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
