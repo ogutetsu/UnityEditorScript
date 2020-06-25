e23d7aa33e26a56c584521508bba13713df8b033
og <oguogu@test.com>
2020-06-25 17:14:16 +0900

    [add] EditorWindowを追加
    シンプルなウインドウを表示
    ホットキーの説明

---
+++ Assets/Editor/EditorWindow/WindowExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WindowExample : EditorWindow
+{
+
+    public static WindowExample instance;
+
+
+    //#_Sはホットキー Shift+S
+    //%...Ctrl / コマンド
+    //#...Shift
+    //LEFT/RIGHT/UP/DOWN...矢印
+    //F1～F12...Fキー
+    //HOME, END, PGUP, PGDN
+    //_X...キー
+    [MenuItem("Custom/Window/Show Palette #_S")]
+    public static void ShowPalette()
+    {
+        instance = EditorWindow.GetWindow(typeof(WindowExample)) as WindowExample;
+        instance.titleContent = new GUIContent("Palette");
+    }
+
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("OnGUI内で描画します。");
+    }
+}
