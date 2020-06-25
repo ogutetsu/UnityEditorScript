74eccfc8bfdab29ef2c54e2dfe063db21f6a550b
og <oguogu@test.com>
2020-06-25 09:34:24 +0900

    [add] ダイアログのサンプルを追加

---
+++ Assets/Editor/Basic/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HelloWorld
+{
+
+    [MenuItem("Custom/Basic/HelloWorld")]
+    private static void HelloDialog()
+    {
+        EditorUtility.DisplayDialog(
+            "Hello World",
+            "ダイアログのサンプルです",
+            "OK"
+        );
+
+    }
+    
+    
+}
