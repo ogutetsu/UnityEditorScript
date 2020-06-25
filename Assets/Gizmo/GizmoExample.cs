7458389dbc826210acf75584edd8ef383f68e268
og <oguogu@test.com>
2020-06-25 09:51:04 +0900

    [add] GizmoExampleを追加

---
+++ Assets/Gizmo/GizmoExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoExample : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        
+    }
+}
