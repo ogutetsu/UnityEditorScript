a5c672bbadd4d1f88ef0a2105b5245c823a57345
og <oguogu@test.com>
2020-06-26 13:34:31 +0900

    [add] CustomSceneViewを追加

---
+++ Assets/Editor/SceneView/CustomSceneEditor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+//選択対象のスクリプト
+[CustomEditor(typeof(CustomSceneExample))]
+public class CustomSceneEditor : Editor
+{
+
+    public enum Type
+    {
+        View,
+        Edit,
+        Test,
+    }
+
+    private int currentType;
+    
+    //シーンビューに描画
+    private void OnSceneGUI()
+    {
+        
+        List<string> typeLabels = new List<string>();
+        foreach (var type in Enum.GetValues(typeof(Type)))
+        {
+            typeLabels.Add(type.ToString());
+        }
+        
+        Handles.BeginGUI();
+        
+        GUILayout.BeginArea(new Rect(10f,10f,200f,20f));
+
+        currentType = GUILayout.Toolbar(
+            (int) currentType,
+            typeLabels.ToArray(),
+            GUILayout.ExpandHeight(true)
+        );
+        
+        GUILayout.EndArea();
+        
+        Handles.EndGUI();
+
+    }
+}
