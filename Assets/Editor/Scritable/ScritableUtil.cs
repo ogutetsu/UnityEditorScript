81d0b684cb3c0291bd842c090ee6447458db54d8
og <oguogu@test.com>
2020-06-29 11:28:01 +0900

    [add] ScritableObjectを追加
    Custom/Scritable/NewDataから新規アセットを作成する機能を実装
    作成したアセットデータを追加

---
+++ Assets/Editor/Scritable/ScritableUtil.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public static class ScritableUtil 
+{
+
+    public static T Create<T>(string path) where T : ScriptableObject
+    {
+        T data = ScriptableObject.CreateInstance<T>() as T;
+        //新しいアセットを作成
+        AssetDatabase.CreateAsset(data, path);
+        //新しいアセットをプロジェクトに表示
+        AssetDatabase.Refresh();
+        //新しく作成されたアセットをプロジェクトに保存
+        AssetDatabase.SaveAssets();
+        return data;
+    }
+    
+    
+    [MenuItem("Custom/Scritable/New Data")]
+    private static void CreateScritable()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("New Data",
+            "ScritableData",
+            "asset",
+            "ScritableObjectのサンプル例");
+        if (path != "")
+            ScritableUtil.Create<ScritableExample>(path);
+    }
+}
---
+++ Assets/Scripts/Scritable/ScritableExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Serializable属性を使用することでクラスのpublicをシリアル化する
+[Serializable]
+public class ScritableExample : ScriptableObject
+{
+    public int hp = 10;
+    public int pow = 5;
+    public float spawnTime = 5.0f;
+    public Sprite background;
+}
