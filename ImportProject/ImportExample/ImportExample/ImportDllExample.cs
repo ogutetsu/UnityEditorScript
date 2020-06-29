d3735bcf4a6cb0867203532a043444f7082c98a2
og <oguogu@test.com>
2020-06-29 15:14:25 +0900

    [add] ImportのDll用プロジェクトを追加

---
+++ ImportProject/ImportExample/ImportExample/ImportDllExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ImportExample
+{
+    public class ImportDllExample : AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPaths)
+        {
+            foreach (var path in importedAssets)
+            {
+                Debug.LogFormat($"ImportDllExample : {path}");
+            }
+        }
+    }
+}
