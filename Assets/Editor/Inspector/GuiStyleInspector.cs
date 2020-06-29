d58a367c236e3b6c997ddef250e36eaf0c85034f
og <oguogu@test.com>
2020-06-29 09:59:22 +0900

    [add] GuiStyleサンプルの追加

---
+++ Assets/Editor/Inspector/GuiStyleInspector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GuiStyleExample))]
+public class GuiStyleInspector : Editor
+{
+
+
+    private GUIStyle _customStyle;
+
+    private void InitStyles()
+    {
+        _customStyle = new GUIStyle();
+        _customStyle.alignment = TextAnchor.MiddleCenter;
+        _customStyle.fontSize = 16;
+    }
+
+    private void OnEnable()
+    {
+        InitStyles();
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        
+        EditorGUILayout.LabelField("boldLabel", EditorStyles.boldLabel);
+        
+        
+        
+    }
+}
