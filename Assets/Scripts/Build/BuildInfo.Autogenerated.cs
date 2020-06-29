07c4d1de28cbabfd7ef0e70da2bc65b16df6774f
og <oguogu@test.com>
2020-06-29 16:34:15 +0900

    [add] コマンドラインからのビルド用スクリプトを追加
    ビルド時の情報表示
    コマンドライン引数から受け取ったテキストの表示を追加

---
+++ Assets/Scripts/Build/BuildInfo.Autogenerated.cs
@@ -0,0 +1,5 @@
+namespace Build{
+	public class BuildInfo {
+		public const string Date = "2020/06/29 16:32:47";
+		public const string Arg = "batファイルからのテキスト";
+	}}
---
+++ Assets/Scripts/Build/BuildUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildUI : MonoBehaviour
+{
+    public Text Date;
+    public Text Arg;
+
+    void Start()
+    {
+        Date.text = "Date : " + Build.BuildInfo.Date;
+        Arg.text = "コマンド引数 : " + Build.BuildInfo.Arg;
+    }
+}
