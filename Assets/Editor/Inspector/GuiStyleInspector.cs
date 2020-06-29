a8e1b43ab421886399d30e20eb4779a70e34435b
og <oguogu@test.com>
2020-06-29 10:30:06 +0900

    [update] カスタムのGUIStyleを追加

--- Assets/Editor/Inspector/GuiStyleInspector.cs
+++ Assets/Editor/Inspector/GuiStyleInspector.cs
@@ -11,11 +11,21 @@ public class GuiStyleInspector : Editor
 
     private GUIStyle _customStyle;
 
+    
+
     private void InitStyles()
     {
         _customStyle = new GUIStyle();
         _customStyle.alignment = TextAnchor.MiddleCenter;
         _customStyle.fontSize = 16;
+        
+        //TextureTypeをGUIに変更する必要があります。
+        Texture2D tex = Resources.Load("Orange") as Texture2D;
+        //normalは通常の表示状態
+        _customStyle.normal.background = tex;
+        _customStyle.normal.textColor = Color.white;
+
+
     }
 
     private void OnEnable()
@@ -28,8 +38,8 @@ public class GuiStyleInspector : Editor
     {
         
         EditorGUILayout.LabelField("boldLabel", EditorStyles.boldLabel);
-        
-        
-        
+        EditorGUILayout.LabelField("Custom", _customStyle);
+
+
     }
 }
