d8fc7da2f5a1a0eaf1203068e81949a47751329f
og <oguogu@test.com>
2020-06-29 11:44:30 +0900

    [add] AssetPostprocessのサンプルを追加

---
+++ Assets/Editor/AssetPostprocessor/TexturePipeline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TexturePipeline : AssetPostprocessor
+{
+    private void OnPreprocessTexture()
+    {
+        Debug.LogFormat($"OnPreprocessTexture : {assetPath}");
+    }
+
+    private void OnPostprocessTexture(Texture2D texture)
+    {
+        Debug.LogFormat($"OnPostprocessTexture : {assetPath}");
+    }
+}
