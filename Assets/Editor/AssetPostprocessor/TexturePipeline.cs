b7b64b465dc5a0506e848676705a1c000f87bdeb
og <oguogu@test.com>
2020-06-29 11:49:56 +0900

    [update] Textureインポード時の設定変更を実装
    Assets/Texture以下にテクスチャをインポートした場合に実行するようにしています。

--- Assets/Editor/AssetPostprocessor/TexturePipeline.cs
+++ Assets/Editor/AssetPostprocessor/TexturePipeline.cs
@@ -14,5 +14,19 @@ public class TexturePipeline : AssetPostprocessor
     private void OnPostprocessTexture(Texture2D texture)
     {
         Debug.LogFormat($"OnPostprocessTexture : {assetPath}");
+
+        //指定のパス下の場合に処理を実行
+        if (assetPath.StartsWith("Assets/Texture"))
+        {
+            //テクスチャの設定を変更
+            TextureImporter importer = assetImporter as TextureImporter;
+            importer.textureType = TextureImporterType.Sprite;
+            TextureImporterSettings settings = new TextureImporterSettings();
+            importer.ReadTextureSettings(settings);
+            settings.spriteAlignment = (int) SpriteAlignment.Center;
+            settings.mipmapEnabled = false;
+            importer.SetTextureSettings(settings);
+        }
+        
     }
 }
