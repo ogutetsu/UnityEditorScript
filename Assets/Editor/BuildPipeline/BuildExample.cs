5d9fd913ad3deb44d38d3e70b1093c436fc1b726
og <oguogu@test.com>
2020-06-29 15:46:12 +0900

    [add] BuildPipelineを追加

---
+++ Assets/Editor/BuildPipeline/BuildExample.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public static class BuildExample
+{
+    private static string buildPath = Application.dataPath + "/../Build";
+
+    public static void CreateBuildFolder()
+    {
+        if (!System.IO.Directory.Exists(buildPath))
+        {
+            System.IO.Directory.CreateDirectory(buildPath);
+        }
+    }
+
+
+    [MenuItem("Custom/Build")]
+    public static void BuildMenu()
+    {
+
+        if (EditorUtility.DisplayDialog("build", "Win64用にビルドしますか?", "OK", "CANCEL"))
+        {
+
+            CreateBuildFolder();
+
+            string[] scenes = GetEnabledScene();
+            string fullPath = buildPath + "/" + BuildTarget.StandaloneWindows64 + "/" + "BuildExample";
+
+            BuildPipeline.BuildPlayer(scenes, fullPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+
+
+            EditorUtility.DisplayDialog("build", "ビルドが完了しました。", "OK");
+
+        }
+    }
+
+
+    private static string[] GetEnabledScene()
+    {
+        List<string> scenes = new List<string>();
+        foreach (var s in EditorBuildSettings.scenes)
+        {
+            if(s.enabled) scenes.Add(s.path);
+        }
+
+        return scenes.ToArray();
+    }
+    
+    
+}
