feb4c52ad27e17419ffecdd981c10e43f116f184
og <oguogu@test.com>
2020-06-29 13:58:29 +0900

    [add] TextPipelineの追加
    テキストファイルのインポートサンプル

---
+++ Assets/Editor/AssetPostprocessor/TextPipeline.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TextPipeline : AssetPostprocessor
+{
+    
+    
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+        string[] movedFromAssetPaths)
+    {
+        foreach (var path in importedAssets)
+        {
+            Debug.LogFormat($"{path}がインポートされました。");
+
+            //拡張子が.txtかチェック
+            if (path.EndsWith(".txt"))
+            {
+                Debug.LogFormat($"テキストファイルがインポートされました。");
+            }
+        }
+            
+    }
+}
