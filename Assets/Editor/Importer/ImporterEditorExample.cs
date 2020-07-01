2df04e008cbe13cc02bb56cbb6cca8facb77eaea
og <oguogu@test.com>
2020-07-01 15:52:51 +0900

    [add] Importerのサンプルを追加

---
+++ Assets/Editor/Importer/ImporterEditorExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+
+//Inspectorに制御パラメータを追加したい場合は、ScriptedImporterEditor.OnInspectorGUIを実装する
+//ScriptedImporterのpublicフィールドでも問題ないが、ScriptedImporterEditorを使った方がわかりやすい
+[CustomEditor(typeof(ImporterExample))]
+public class ImporterEditorExample : ScriptedImporterEditor
+{
+    public override void OnInspectorGUI()
+    {
+        var scale = new GUIContent("Scale");
+        //Custom対象のクラスのプロパティ名を指定します
+        var prop = serializedObject.FindProperty("scale");
+        EditorGUILayout.PropertyField(prop, scale);
+        
+        //最後に呼び出す必要がある
+        base.ApplyRevertGUI();
+    }
+}
---
+++ Assets/Editor/Importer/ImporterExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+
+//拡張子 cubeのアセットをインポートする
+[ScriptedImporter(1, "cube")]
+public class ImporterExample : ScriptedImporter
+{
+    public float scale = 1;
+    //拡張子cubeのファイルがインポートされると、cubeのPrimitiveとマテリアルを追加したprefabが作成されます。
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        cube.transform.position = Vector3.zero;
+        cube.transform.localScale = new Vector3(scale, scale, scale);
+
+        ctx.AddObjectToAsset("MainAsset", cube);
+        ctx.SetMainObject(cube);
+        
+        var material = new Material(Shader.Find("Standard"));
+        
+        ctx.AddObjectToAsset("My Material", material);
+        
+
+    }
+}
