766e047c922dffb8d5a786dcd1bb740fb4a1b860
og <oguogu@test.com>
2020-07-01 16:15:28 +0900

    [add] UIElemets用のCustomWindowを追加

---
+++ Assets/Editor/UIElements/UIElementsWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIElementsWindow : EditorWindow
+{
+
+    [MenuItem("Custom/UIElements/Window")]
+    public static void ShowWindow() => GetWindow<UIElementsWindow>("UIElementsExample");
+
+    private void OnEnable()
+    {
+        var root = rootVisualElement;
+        
+        var slider = new SliderInt();
+        root.Add(slider);
+        
+        var box = new Box();
+        box.Add(new Label(){text = "ラベルの表示"});
+        box.Add(new Button(){text = "ボタンです"});
+        
+        root.Add(box);
+       
+
+    }
+}
