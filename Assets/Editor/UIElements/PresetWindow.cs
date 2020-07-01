2b285d2d363d7b956fd30cc62f4651c1601f59f4
og <oguogu@test.com>
2020-07-01 16:58:15 +0900

    [add] PresetWIndowを追加
    ussのサンプルも追加

---
+++ Assets/Editor/UIElements/PresetWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PresetWindow : EditorWindow
+{
+
+    [MenuItem("Custom/UIElements/PresetWindow")]
+    public static void ShowWindow() => GetWindow<PresetWindow>("Preset");
+
+    private void OnEnable()
+    {
+        var root = rootVisualElement;
+        
+        var uxml = Resources.Load<VisualTreeAsset>("PresetWindow");
+        
+        var styleSheet = Resources.Load<StyleSheet>("PresetWindowUSS");
+        root.styleSheets.Add(styleSheet);
+        uxml.CloneTree(root);
+
+    }
+
+
+    private void OnGUI()
+    {
+        rootVisualElement.Q<VisualElement>("Container").style.height = new StyleLength(position.height);
+    }
+}
